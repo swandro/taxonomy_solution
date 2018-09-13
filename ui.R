f5a1ce5e830dcc41f50e5ac0ffa30a883ead94b6
Stephen Wandro <stevewandro@gmail.com>
2018-09-12 17:55:20 -0700

    working version

--- ui.R
+++ ui.R
@@ -1,12 +1,22 @@
 fluidPage(
-  titlePanel("Taxonomy Solution"),
+  titlePanel("Stephen's taxonomy barplots"),
   sidebarLayout(
-    sidebarPanel(
+    sidebarPanel(h2("Input options"),
       fileInput(inputId = "file1", 
                 label = "Upload OTU table", 
-                buttonLabel = "Upload")
+                buttonLabel = "Upload"),
+      checkboxInput(inputId = "SAMPLES.IN.ROWS", label= "Samples in rows", value=TRUE),
+      selectInput(inputId = "TaxLev",
+                  label = "Taxonomic Level",
+                  choices = c("L1","L2","L3","L4","L5","L6","L7"),
+                  selected = "L2"),
+      actionButton("graph.button","Plot"),
+      numericInput(inputId= "num.taxa", label = "Number of taxa to plot", value = 4,min = 1, max=12),
+      numericInput(inputId= "X.size", label = "Size of X-axis font", value = 10,min = 2, max=50),
+      downloadButton(outputId= "Download", label="Download .tsv")
     ),
-    mainPanel(
-      plotOutput(outputId = "graph"), width = 8)
+    mainPanel(h2("Taxa to plot"),
+        tableOutput(outputId = "top.taxa.table"),
+        plotOutput(outputId = "graph"), width=35)
   )
 )
